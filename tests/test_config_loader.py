4821cdb57677a2af54a3b32aca9c1be37ca16872
Michail19 <Mi.ershov.2017@yandex.ru>
2024-11-04 21:21:39 +0300

    Tests update

--- tests/test_config_loader.py
+++ tests/test_config_loader.py
@@ -4,7 +4,7 @@ from src.config_loader import load_config
 
 class TestConfigLoader(unittest.TestCase):
     def test_load_config(self):
-        config = load_config('config.xml')
+        config = load_config('D:\\Python\\HomeworkForKU2\\config.xml')
         self.assertEqual(config['graphviz_path'], 'D:\\Programs\\Graphviz\\bin\\dot.exe')
         self.assertEqual(config['repo_path'], 'D:\\Python\\HomeworkForKU2')
         self.assertEqual(config['output_path'], 'D:\\Python\\HomeworkForKU2\\output\\output.png')
